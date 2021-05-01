2677dbaea02beb72be55600085d01e1fd7c06041
Mohit1098 <mohitsingla1098@gmail.com>
2021-05-01 18:48:46 +0530

    Adding main file

--- main.cu
+++ main.cu
@@ -5,4 +5,61 @@ using namespace std;
 #define ve vector
 #define vi vector<int>
 
-ve<pairi> LSH(ve<vi>)
+
+class mkDSU{
+ public:
+	vector<int>par,size;
+
+	mkDSU(int n){
+		par.resize(n);
+		size.resize(n);
+
+		FOR(i,0,n){
+			par[i]=i;
+			size[i]=0;
+		}
+	}
+
+	int find(int a){
+		int p=a,t;
+		while(par[p]!=p)
+			p=par[p];
+
+		while(p!=a){
+			t=par[a];
+			par[a]=p;
+			a=t;
+		}
+		return a;
+	}
+
+	void union_(int a,int b){
+		int u1=find(a);
+		int u2=find(b);
+
+		if(u1 == u2)
+			return ;
+
+		if(size[u1]<size[u2]){
+			par[u1]=u2;
+		}else{
+			if(size[u2]<size[u1]){
+				par[u2]=u1;
+			}
+			else{
+				par[u1]=u2;
+				size[u2]++;
+			}
+		}
+	}
+};
+
+
+ve<pairi> LSH(ve<vi>&S, int siglen, int bsize){
+
+
+}
+
+vi reordered_rows(ve<vi>&S){
+
+}
