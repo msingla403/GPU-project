3c38bc59333c3910eea865c1049a8d6f482a4830
Harshit Kedia <kediaharshit9@gmail.com>
2021-05-08 18:47:54 +0530

    detecting dense cols

--- main.cu
+++ main.cu
@@ -143,9 +143,53 @@ set<pairi> LSH(vi &rowptr, vi &colidx, int siglen, int bsize, int numbuckets){
 	return result;
 }
 
-// struct posi{
-// 	int r, c;
-// };
+#define PANEL_SIZE 3
+#define DENSE_THRESHOLD 2
+
+__global__ void SPMM(int * tile_row_ptr, int * panel_ptr, int * col_val, int * col_idx){
+
+	int row_panel_id = blockIdx.x;
+	int row_id = threadIdx.x/32;
+	int thread_no = threadIdx.x%32;
+
+	int num_tiles = panel_ptr[row_panel_id+1] - panel_ptr[row_panel_id];
+
+	int ptr = panel_ptr[row_panel_id]*PANEL_SIZE + row_id*num_tiles;
+
+	for(int i=0;i<num_tiles;++i){
+
+		int low = tile_row_ptr[ptr+i];
+		int high = tile_row_ptr[ptr+i+1];
+
+		if(high>low){
+			int j=low;
+			O[row_id][thread_no] += col_val[j] * D[col_idx[j]][thread_no];
+		}
+	}
+}
+
+__global__ find_dense(int *col_ptr, int* row_idx, int *isdense, int nr, int nc)
+{	
+	int idx = blockIdx.x*blockDim.x + threadIdx.x;
+	if(idx < (nr/PANEL_SIZE)*nc)
+	{
+		int panel_id = idx/nc;
+		int col_id = idx%nc;
+		
+		int counter = 0;
+		for(int i=col_ptr[col_id]; i<col_ptr[col_id+1]; i++)
+		{
+			if(row_index[i] >= panel_id*PANEL_SIZE && row_idx[i] < (panel_id+1)*PANEL_SIZE)
+				counter++;
+		}
+
+		if(counter >= DENSE_THRESHOLD)
+			isdense[idx] = 1;
+		else
+			isdense[idx] = 0;
+	}
+
+}
 
 int main(int argc, char** argv)
 {
@@ -157,12 +201,18 @@ int main(int argc, char** argv)
 	fscanf(fp, "%d %d %d", &nr, &nc, &ne);
 
 	int row_ptr[nr+1];
+	int col_idx[ne];
 	for(int i=0; i<=nr; i++)
 		row_ptr[i] = 0;
 
-	int col_idx[ne];
 	int col_val[ne];
 
+
+	int col_ptr[nc+1];
+	int row_idx[ne];
+	for(int i=0; i<=nr; i++)
+		col_ptr[i] = 0;
+		
 	int r, c;
 	
 	for(int i=0; i<ne; i++)
@@ -171,21 +221,43 @@ int main(int argc, char** argv)
 		
 		row_ptr[r]++;
 		col_idx[i] = c-1;
-		col_val[i] = 1;		
+		col_val[i] = 1;
+		
+		col_ptr[c]++;
+		row_idx[i] = r-1;
 	}
 	for(int i=0; i<nr; i++)
 		row_ptr[i+1] += row_ptr[i];
+
+	for(int i=0; i<nc; i++)
+		col_ptr[i+1] += col_ptr[i];
 	
-	for(int i=0; i<=nr; i++)
-	{
-		cout << row_ptr[i] << " ";
-	}
-	cout <<endl;
+	// for(int i=0; i<=nr; i++)
+	// 	cout << row_ptr[i] << " ";
+	
+	// cout <<endl;
 
-	for(int i=0; i<ne; i++)
-		cout << col_idx[i] << " ";
-	cout << endl;
+	// for(int i=0; i<ne; i++)
+	// 	cout << col_idx[i] << " ";
+	// cout << endl;
+	int num_panels = nr/PANEL_SIZE;
+	int thr = num_panels*nc;
 
+	int *dcol_ptr;
+	int *drow_idx;
+	int *is_dense;
+	cudaMalloc(&dcol_ptr, (nr+1)*sizeof(int));
+	cudaMalloc(&drow_idx, ne*sizeof(int));
+	cudaMalloc(&is_dense, thr*sizeof(int));
+
+	cudaMemcpy(dcol_ptr, col_ptr, (nr+1)*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(drow_idx, row_idx, ne*sizeof(int), cudaMemcpyHostToDevice);
+
+	find_dense<<< (thr+1023)/1024, 1024>>>(dcol_ptr, drow_idx, is_dense, nr, nc);
+
+	int isdense[thr];
+	cudaMemcpy(isdense, is_dense, thr*sizeof(int), cudaMemcpyDeviceToHost);
+	
 	// int n = 6;
 	// int m = 6;
 	// vi rowptr{0,2,5,7,8,11,13};
